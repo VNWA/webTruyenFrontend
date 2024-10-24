a47e21f7ca901c33c517d0fcfca68e0b4dd00470
VNWA <nhatnguyen061003@gmail.com>
2024-10-25 04:56:45 +0700

    two commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,11 +31,11 @@ export default defineNuxtConfig({
     format: ['webp']
   },
   site: {
-    url: 'https://localhost:300',
+    url: 'https://main.d3iqgblvocdgo2.amplifyapp.com',
   },
   runtimeConfig: {
     public: {
-      apiBase: 'http://127.0.0.1:8000/api/v1',
+      apiBase: 'https://demo1.vinawebapp.com/api/v1',
     },
   },
   css: [
